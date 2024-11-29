51dd45d83831c3ef264b3f581124524963575e9a
Xander Hill <xander.k.hill@gmail.com>
2024-11-29 15:26:34 -0600

    MangoDB frontend and backend integration

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const Note = require('./models/note')
 
 app.use(express.json())
 app.use(cors())
@@ -29,18 +32,15 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes',  (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -49,14 +49,6 @@ app.delete('/api/notes/:id', (request, response) => {
 
     response.status(204).end()
 })
-
-const generateId = () => {
-    const maxId = notes.length > 0
-        ? Math.max(...notes.map(n => Number(n.id)))
-        : 0
-
-    return String(maxId + 1)
-}
 
 app.post('/api/notes', (request, response) => {
     const body = request.body
@@ -67,15 +59,14 @@ app.post('/api/notes', (request, response) => {
         })
     }
     
-    const note = {
+    const note = new Note({
         content: body.content,
         important: Boolean(body.important) || false,
-        id: generateId()
-    }
+    })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+    note.save().then(savedNote=> {
+        response.json(savedNote)
+    })
 })
 
 const requestLogger = (request, response, next) => {
@@ -94,7 +85,7 @@ const unknownEndpoint = (request, response) => {
   
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
