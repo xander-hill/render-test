c1e13d3681da062776bc37e30ccb6369ea161067
Xander Hill <xander.k.hill@gmail.com>
2024-11-30 13:54:05 -0600

    directory structure altered

--- models/note.js
+++ models/note.js
@@ -1,22 +1,4 @@
 const mongoose = require('mongoose')
-require('dotenv').config();
-
-mongoose.set('strictQuery', false)
-
-
-const url = process.env.MONGODB_URI
-
-
-console.log('connecting to', process.env.MONGODB_URIl)
-
-mongoose.connect(url)
-
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch(error => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
 
 const noteSchema = new mongoose.Schema({
   content: {
--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -14,17 +14,17 @@ mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+  content: String,
+  important: Boolean,
 })
 
 const Note = mongoose.model('Note', noteSchema)
 
-Note.find({}).then(result=> {
-    result.forEach(note => {
-        console.log(note)
-    })
-    mongoose.connection.close()
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
 })
 
 // const note = new Note({
