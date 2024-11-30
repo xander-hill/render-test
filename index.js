a521968864569956ba7a64841a1a675b52ed09d3
Xander Hill <xander.k.hill@gmail.com>
2024-11-29 19:20:57 -0600

    Better error handling

--- index.js
+++ index.js
@@ -67,10 +67,10 @@ app.delete('/api/notes/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
 
-    if (!body.content) {
+    if (body.content === undefined) {
         return response.status(400).json({
             error: 'content missing'
         })
@@ -81,20 +81,20 @@ app.post('/api/notes', (request, response) => {
         important: Boolean(body.important) || false,
     })
 
-    note.save().then(savedNote=> {
-        response.json(savedNote)
-    })
+    note.save()
+        .then(savedNote=> {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-    const body = request.body
+    const {content, important} = request.body
 
-    const note = {
-        content: body.content,
-        important: body.important
-    }
-
-    Note.findByIdAndUpdate(request.params.id, note, {new: true})
+    Note.findByIdAndUpdate(
+        request.params.id, 
+        {content, important}, 
+        {new: true, runValidators: true, context: 'qurery'})
         .then(updatedNote => {
             response.json(updatedNote)
         })
@@ -112,6 +112,8 @@ const errorHandler = (error, request, response, next) => {
 
     if(error.name === 'CastError') {
         return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
 
     next(error)
