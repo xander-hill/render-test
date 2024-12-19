ee795aea5e560e70fe02c14595c6931506ae71c5
Xander Hill <xander.k.hill@gmail.com>
2024-12-18 21:00:48 -0600

    can't remember

--- utils/middleware.js
+++ utils/middleware.js
@@ -18,7 +18,7 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastErrror') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).jsonn({ error: error.message })
+    return response.status(400).json({ error: error.message })
   } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
     return response.status(400).json({ error: 'expected `username` to be unique' })
   } else if (error.name ===  'JsonWebTokenError') {
